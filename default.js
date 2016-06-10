7b9133d84d618462c6282f117d510ea57861ff34
Tom Clarkson <tom@orangeguava.com>
2016-06-10 15:13:11 +1000

    more complete rules

--- default.js
+++ default.js
@@ -3,17 +3,222 @@
 "use strict";
 
 module.exports = {
-    "extends": "defaults/configurations/eslint",
+    "extends": "eslint:recommended",
     "parser": "babel-eslint",
-    "rules":{
-        "eqeqeq": 0,
+    "rules": {
+
+        "no-array-constructor": "warn",
+
+        "no-bitwise": "error",
+
+        "no-caller": "error",
+        "no-catch-shadow": "warn",
+        "no-confusing-arrow": "off",
+
+        "no-duplicate-imports": "error",
+
+        // noop is useful
+        "no-empty-function": "off",
+
+        "no-eval": "error",
+
+        "no-extend-native": "warn",
+
+        "no-extra-bind": "warn",
+        "no-extra-label": "error",
+        "no-floating-decimal": "warn",
+
+        "no-implicit-globals": "warn",
+
+        "no-implied-eval": "error",
+
+        // doesn't work too well with function params
+        "no-invalid-this": "off",
+
+        "no-iterator": "error",
+        "no-lone-blocks": "error",
+
+        "no-loop-func": "warn",
+        "no-mixed-requires": "warn",
+        "no-multi-spaces": "warn",
+
+        "no-native-reassign": "error",
+        "no-nested-ternary": "warn",
+        "no-new": "error",
+
+
+        "no-proto": "error",
+
+        "no-return-assign": "warn",
+        "no-script-url": "error",
+        "no-self-assign": "error",
+        "no-self-compare": "error",
+
+        "dot-location": ["error", "property"],
+        "no-throw-literal": "error",
+        "no-undef-init": "error",
+        "no-unmodified-loop-condition": "error",
+
+        "no-unsafe-finally": "error",
+        "no-spaced-func": "error",
+
+
+        "no-whitespace-before-property": "error",
+
+
+        "no-use-before-define": "error",
+        "no-useless-call": "error",
+        "no-useless-computed-key": "error",
+        "no-useless-constructor": "warn",
+        "no-useless-escape": "error",
+
+        // this rule seems broken
+        "no-useless-rename": "off",
+
+        "no-void": "error",
+
+        "no-warning-comments": "warn",
+        "no-with": "error",
+
+
+        "array-bracket-spacing": "warn",
+        "array-callback-return": "error",
+
+
+        "accessor-pairs": "error",
+        "block-scoped-var": "warn",
+
+        // false positives
+        "callback-return": "off",
+
+        "comma-spacing": "warn",
+        "comma-style": "error",
+        "computed-property-spacing": "error",
+
+        // this rule doesn't like return callback()
+        "consistent-return": "off",
+
+
+        "default-case": "error",
+
+        "generator-star-spacing": "error",
+        "handle-callback-err": ["error", "err"],
+
+        "key-spacing": "warn",
+        "keyword-spacing": "warn",
+
+        "max-depth": "warn",
+        "max-nested-callbacks": "warn",
+
+        "max-params": ["warn", 10],
+
+        "new-parens": "error",
+
+
+        // "no-mixed-operators": "off", ???
+        "block-spacing": "error",
+
+        "eol-last": "off",
+
+        // useful for debugging?
+        "func-names": "off",
+        // ?
+        "func-style": "off",
+
+        // "object-curly-newline": "off", ??
+        // "object-curly-spacing": ["off", "never"], ??
+
+
+        "operator-assignment": "warn",
+        // "rest-spread-spacing": "off", ???
+        "semi-spacing": "error",
+        "space-before-blocks": "warn",
+
+        "space-unary-ops": "warn",
+
+        "template-curly-spacing": "error",
+        "yield-star-spacing": "error",
+
+
+
+
+        "no-new-func": "error",
+        "no-new-require": "error",
+        "no-new-wrappers": "error",
+        "no-octal-escape": "error",
+        "no-label-var": "error",
+
+
+        "no-path-concat": "warn",
+
+        "no-process-env": "warn",
+
+        "no-process-exit": "warn",
+        "no-sequences": "warn",
+        "no-shadow": "warn",
+        "no-shadow-restricted-names": "error",
+
+
+        //"one-var": ["warn", "never"],
+        "one-var-declaration-per-line": "warn",
+
+
+        "require-yield": "off",
+        "radix": "off",
+        "require-jsdoc": "off",
+        "no-eq-null": "off",
+
+
+        "no-new-object": "error",
+        // "complexity": ["off", 11],
+
+        // too many errors
+        "no-magic-numbers": "off",
+        "max-len": "off",
+        "no-negated-condition": "off",
+        "prefer-arrow-callback": "off",
+        "prefer-const": "off",
+        "prefer-reflect": "off",
+        "prefer-rest-params": "off",
+        "prefer-spread": "off",
+        "prefer-template": "off",
+        "spaced-comment": "off",
+        "no-param-reassign": "off",
+        "max-statements-per-line": "off",
+        "valid-jsdoc": "off",
+        "no-useless-concat": "off",
+        "object-shorthand": "off",
+        "no-lonely-if": "off",
+        "newline-per-chained-call": "warn",
+
+        "operator-linebreak": ["off", "before"],
+        "camelcase": "off",
+
+
+        // optional style stuff
+        "space-before-function-paren": "warn",
+        "padded-blocks": ["off", "never"],
+        "object-property-newline": "off",
+        "brace-style": "off",
+        "space-in-parens": "warn",
+        "space-infix-ops": "warn",
+
+        // triggers on done && done()
+        "no-unused-expressions": "off",
+
+
+
+        "no-unneeded-ternary": "warn",
+
+        "no-underscore-dangle": ["warn", {"allow": ["_id"]}],
+
+
         "no-console": 0,
-        "no-unused-vars": [2, {"vars": "all", "args":"none"}],
+        "no-unused-vars": [2, {"vars": "all", "args": "none"}],
         "indent": [
             2,
             4
         ],
-        "quotes": 0,
         "linebreak-style": [
             2,
             "unix"
@@ -22,21 +227,28 @@ module.exports = {
             2,
             "always"
         ],
-        // strict mode isn't really needed in es6 code
-        "strict": 0, //[2, "never"],
-        //"dot-notation": [2, {"allowKeywords": false}],
 
-        // allow one line ifs
-        "curly": 0,
-        // newline at file end is optional
-        "eol-last": 0,
-        // allow alerts
-        "no-alert": 2,
+        // disallow alerts
+        "no-alert": "error",
+
         // new-cap does not recognize decorators as new
-        "new-cap": 0,
-        "no-labels": 0,
-        "no-this-before-super": 2,
-        "no-trailing-spaces": 2
+        "new-cap": "off",
+
+        "no-constant-condition": "warn",
+        "arrow-spacing": "warn",
+
+        // "global-require": "warn",
+
+        "strict": "warn",
+
+        "yoda": "error",
+
+        "unicode-bom": "error",
+
+        "no-trailing-spaces": "error"
+
+
+
     },
     "globals": {
     },
