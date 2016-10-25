49e14c3252ecd37409657992fbc2ca7d1afd7fa6
Tom Clarkson <tom@orangeguava.com>
2016-10-26 10:57:42 +1100

    add react config

---
+++ react.js
@@ -0,0 +1,18 @@
+// default eslint settings for a React app
+
+module.exports = {
+    "extends": ["tqc/default", "plugin:react/recommended"],
+    "plugins": ["react"],
+    "rules": {
+    },
+    "globals": {
+    },
+    "env": {
+        "es6": true,
+        "browser": true,
+        "node": true
+    },
+    "ecmafeatures": {
+      "jsx": true
+    }
+};
